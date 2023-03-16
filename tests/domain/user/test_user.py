3129dcdfe7df66219292571d25d0daf54bc5b6a6
DaniNicuta <dani_nicuta@yahoo.com>
2023-03-16 21:04:59 +0200

    Add domain user class

--- tests/domain/user/test_user.py
+++ tests/domain/user/test_user.py
@@ -4,24 +4,21 @@ from domain.user.user import User
 
 
 class UserMyTestCase(unittest.TestCase):
-    def test_user_sets_the_right_username(self):
+    def test_user_sets_right_username(self):
         # set up
-        username = "random_generated"
+        username = "random_generate"
         user = User(username)
         # execution
         actual_username = user.username
         # assertion
         self.assertEqual(username, actual_username)
 
+    @unittest.skip("TODO: Homework")
     def test_it_sets_empty_list_if_we_do_not_specify_stock(self):
-        user = User("random-username")
+        pass
 
-        actual_stocks = user.stocks
-
-        self.assertEqual([], actual_stocks)
-
-    @unittest.skip("TODO: homework")
-    def test_it_sets_the_stocks_we_give(self):
+    @unittest.skip("TODO: Homework")
+    def test_it_sets_the_stock_we_give(self):
         # set a list of 3 strings
         pass
 
