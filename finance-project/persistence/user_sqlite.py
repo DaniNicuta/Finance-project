953c149d2c112df06909c1d51e0c7b8547593111
DaniNicuta <dani_nicuta@yahoo.com>
2023-04-13 18:11:30 +0300

    Refactor get_all

--- finance-project/persistence/user_sqlite.py
+++ finance-project/persistence/user_sqlite.py
@@ -1,5 +1,5 @@
 import sqlite3
-from domain.user.persistance_interface import UserPersistenceInterface
+from domain.user.persistace_interface import UserPersistenceInterface
 from domain.user.user import User
 from domain.user.factory import UserFactory
 
@@ -8,7 +8,14 @@ class UserPersistenceSqlite(UserPersistenceInterface):
     def get_all(self) -> list[User]:
         with sqlite3.connect("main_users.db") as conn:
             cursor = conn.cursor()
-            # TODO homework, try except return empty list if no db
+            # TODO homework, try except return empty list if no db(model mai jos)
+            try:
+                cursor.execute("SELECT * FROM users")
+            except sqlite3.OperationalError as e:
+                if "no such table" in str(e):
+                    return []
+                else:
+                    raise e
             cursor.execute("SELECT * FROM users")
             users_info = cursor.fetchall()
         factory = UserFactory()
@@ -19,11 +26,31 @@ class UserPersistenceSqlite(UserPersistenceInterface):
         with sqlite3.connect("main_users.db") as conn:
             cursor = conn.cursor()
             try:
-                cursor.execute(f"INSERT INTO users (id, username) VALUES ('{user.id}','{user.username}')")
+                cursor.execute(
+                    f"INSERT INTO users (id, username) VALUES ('{user.id}','{user.username}')"
+                )
             except sqlite3.OperationalError as e:
                 if "no such table" in str(e):
-                    cursor.execute("CREATE TABLE users (id TEXT PRIMARY KEY, username TEXT NOT NULL)")
+                    cursor.execute(
+                        "CREATE TABLE users (id TEXT PRIMARY KEY, username TEXT NOT NULL)"
+                    )
                 else:
                     raise e
-                cursor.execute(f"INSERT INTO users (id, username) VALUES ('{user.id}','{user.username}')")
+                cursor.execute(
+                    f"INSERT INTO users (id, username) VALUES ('{user.id}','{user.username}')"
+                )
+            conn.commit()
+
+    def delete(self, uuid: int):
+        with sqlite3.connect("main_users.db") as conn:
+            cursor = conn.cursor()
+            cursor.execute(f"DELETE FROM users WHERE id = {uuid}")
+            conn.commit()
+
+    def edit(self, user: User):
+        with sqlite3.connect("main_users.db") as conn:
+            cursor = conn.cursor()
+            cursor.execute(
+                f"UPDATE users SET username = '{user.username}' where id = '{user.id}'"
+            )
             conn.commit()
